2bed2c945886ff8977765320fb906c5acfe00474
iccanfly <iccanfly@gmail.com>
2026-06-14 19:38:43 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,1 +1,9 @@
+def cari_pasangan(arr, target):
+    seen = set()
+    for num in arr:
+        complement = target - num
+        if complement in seen:
+            return True
+        seen.add(num)
+    return False
 
