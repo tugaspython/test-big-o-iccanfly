268bd31295a8796ae488d08998a58b8dabc871df
iccanfly <iccanfly@gmail.com>
2026-06-16 19:34:25 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,8 +1,8 @@
-import re
-
-def validasi_password(password):
-    return (len(password) >= 8 and
-            ' ' not in password and
-            bool(re.search(r'[A-Z]', password)) and
-            bool(re.search(r'\d', password)) and
-            bool(re.search(r'[!@#$%^&*()_+\-=\[\]{};:\'",.<>?/\\|`~]', password)))
+def cari_pasangan(arr, target):
+    seen = set()
+    for num in arr:
+        complement = target - num
+        if complement in seen and complement != num:
+            return True
+        seen.add(num)
+    return False
