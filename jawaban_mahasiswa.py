5c634fab6ff99daf41d65cc5ef3ccfb33ae51830
iccanfly <iccanfly@gmail.com>
2026-06-16 19:53:27 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,12 +1,15 @@
 def cari_pasangan(arr, target):
+    """Mengembalikan True jika ada dua angka berbeda yang jumlahnya = target."""
     if len(arr) < 2:
         return False
 
     seen = set()
     seen_add = seen.add
+
     for num in arr:
         complement = target - num
         if complement in seen and complement != num:
             return True
         seen_add(num)
+
     return False
